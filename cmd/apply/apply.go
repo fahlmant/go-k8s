9ff1537788d9d5b990b77796e37dfde560362aa1
Taylor Fahlman <tfahlman@redhat.com>
2018-08-21 15:00:22 -0400

    Decode file passed in via flags, switch on type of resource

--- cmd/apply/apply.go
+++ cmd/apply/apply.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"flag"
-	//"fmt"
+	"fmt"
+    "log"
+    "io/ioutil"
+    "reflect"
     //"time"
     //"github.com/ghodss/yaml"
     //"k8s.io/apimachinery/pkg/api/errors"
     //"github.com/pkg/errors"
-	//appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -29,8 +32,29 @@ func init() {
     flag.StringVar(configFlag, "f", "", "Specify the k8s object config file")
 }
 
+func decodeFile(*string) {
+
+    yamlFile, _ := ioutil.ReadFile("test-namespace.yaml")
+    decode := scheme.Codecs.UniversalDeserializer().Decode
+	obj, _, err := decode([]byte(yamlFile), nil, nil)
+    if err != nil {
+	    log.Fatal(fmt.Sprintf("Error while decoding YAML object. Err was: %s", err))
+	}
+    fmt.Printf("%++v\n\n", obj.GetObjectKind())
+    fmt.Printf("%++v\n\n", obj)
+    /*switch o := obj.(type) {
+        case *apiv1.Namespace:
+             _, err = clientset.CoreV1().Namespaces().Create(o)
+        default:
+            fmt.Printf("bar")
+    }*/
+
+}
+
+
 func main() {
 
+  //  processFile()
     home := homedir.HomeDir()
 	var kubeconfig string = filepath.Join(home, ".kube", "config")
 	flag.Parse()
@@ -43,11 +67,27 @@ func main() {
 	if err != nil {
 		panic(err)
     }
-    nsSpec := &apiv1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns"}}
+    /*nsSpec := &apiv1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns"}}
     _, err = clientset.CoreV1().Namespaces().Create(nsSpec)
     if err != nil {
         panic(err)
+    }*/
+    yamlFile, _ := ioutil.ReadFile(*configFlag)
+    decode := scheme.Codecs.UniversalDeserializer().Decode
+    obj, _, err := decode([]byte(yamlFile), nil, nil)
+    if err != nil {
+        log.Fatal(fmt.Sprintf("Error while decoding YAML object. Err was: %s", err))
     }
+    fmt.Printf("%++v\n\n", obj.GetObjectKind())
+    fmt.Printf("%++v\n\n", obj)
+    switch o := obj.(type) {
+        case *apiv1.Namespace:
+            fmt.Printf("Foo\n")
+             _, err = clientset.CoreV1().Namespaces().Create(o)
+        default:
+            fmt.Printf("bar")
+    }
+    fmt.Println(reflect.TypeOf(obj))
 }
 
 
